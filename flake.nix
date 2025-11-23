31b62a93f6d240bd0326061bf22f6dcf269e0bda
Ante Petrovic <petrovicante6@gmail.com>
2025-11-23 14:03:57 +0100

    stylix fix

--- flake.nix
+++ flake.nix
@@ -19,84 +19,79 @@
         "x86_64-darwin"
       ];
 
-      perSystem =
-        {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          lib,
-          ...
-        }:
-        {
-          packages.default =
-            (wrappers.wrapperModules.helix.apply {
+     perSystem = { pkgs, lib,  system, ... }:
+let
+  tomlFmt = pkgs.formats.toml { };
 
-              config = {
-                inherit pkgs;
-                themes = {
-                  theme = "nord";
-                };
+  # ---- single source of truth for Helix settings ----
+  mySettings = {
+    theme = "stylix";
+    editor = {
+      bufferline = "multiple";
+      "color-modes" = true;
+      "true-color" = true;
+      "auto-pairs" = false;
+      "line-number" = "relative";
+      "inline-diagnostics" = { "cursor-line" = "hint"; };
+      "cursor-shape" = { insert = "bar"; normal = "block"; select = "underline"; };
+      statusline = {
+        left  = [ "mode" "spinner" "file-name" ];
+        right = [ "diagnostics" "selections" "position" "file-encoding" "file-line-ending" "file-type" ];
+        separator = "│";
+      };
+    };
+    keys = {
+      normal = {
+        # Lazygit
+        "C-g" = [
+          ":new"
+          ":insert-output ${lib.getExe pkgs.lazygit}"
+          ":buffer-close!"
+          ":redraw"
+        ];
 
-                settings = {
-                  theme = "nord-night";
-                  editor = {
-                    bufferline = "multiple";
-                    "color-modes" = true;
-                    "true-color" = true;
-                    "auto-pairs" = false;
-                    "line-number" = "relative";
+        # Yazi
+        "C-y" = [
+          ":sh rm -f /tmp/unique-file"
+          ":insert-output ${lib.getExe pkgs.yazi} %{buffer_name} --chooser-file=/tmp/unique-file"
+          # reset alt-screen + bracketed paste in TTY
+          ":insert-output echo \"\\x1b[?1049h\\x1b[?2004h\" > /dev/tty"
+          ":open %sh{cat /tmp/unique-file}"
+          ":redraw"
+        ];
+      };
+    };
+  };
 
-                    "inline-diagnostics" = {
-                      "cursor-line" = "hint";
-                    };
+  # generate a TOML file in the store for -c
+  cfgPath = tomlFmt.generate "helix-config" mySettings;
 
-                    "cursor-shape" = {
-                      insert = "bar";
-                      normal = "block";
-                      select = "underline";
-                    };
+  # your original wrapper (do NOT force XDG_CONFIG_HOME to a store path)
+  wrapped = inputs.wrappers.wrapperModules.helix.apply {
+    config = {
+      inherit pkgs;
+      settings = mySettings;
 
-                    statusline = {
-                      left = [
-                        "mode"
-                        "spinner"
-                        "file-name"
-                      ];
-                      right = [
-                        "diagnostics"
-                        "selections"
-                        "position"
-                        "file-encoding"
-                        "file-line-ending"
-                        "file-type"
-                      ];
-                      separator = "│";
-                    };
-                  };
+      # make sure Helix uses the real user config dir at runtime
+       env.XDG_CONFIG_HOME = lib.mkForce "\$HOME/.config";
+      # (omit HELIX_RUNTIME unless you truly need it)
+    };
+  };
 
-                  keys = {
-                    normal = {
-                      "C-g" = [
-                        ":new"
-                        ":insert-output ${lib.getExe pkgs.lazygit}"
-                        ":buffer-close!"
-                        ":redraw"
-                      ];
-
-                      "C-y" = [
-                        '':sh rm -f /tmp/unique-file ''
-                        '':insert-output ${lib.getExe pkgs.yazi} %{buffer_name} --chooser-file=/tmp/unique-file ''
-                        '':insert-output echo "\x1b[?1049h\x1b[?2004h" > /dev/tty ''
-                        '':open %sh{cat /tmp/unique-file} ''
-                        ":redraw"
-                      ];
-                    };
-                  };
-                };
-              };
-            }).wrapper;
-        };
+  # re-wrap hx to always pass -c <store-config>
+hxWithC = pkgs.symlinkJoin {
+  name = "helix-with-wrapper-config";
+  paths = [ wrapped.wrapper ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    wrapProgram "$out/bin/hx" \
+      --unset HELIX_RUNTIME \
+      --set XDG_CONFIG_HOME "$HOME/.config" \
+      --add-flags "-c ${cfgPath}"
+  '';
+};in {
+  packages.default = hxWithC;
+}
+;
     };
 }
